db2c136724f31e1d0cc204093b8d1287e3349382
Rybov <jankowalczyk38@gmail.com>
2020-10-27 19:06:04 +0100

    TODO Removed

--- triss-core/src/main/kotlin/com/pp/trisscore/controller/ApplicationController.kt
+++ triss-core/src/main/kotlin/com/pp/trisscore/controller/ApplicationController.kt
@@ -17,10 +17,10 @@ import reactor.core.publisher.Mono
 @RequestMapping("/application")
 class ApplicationController(val applicationService: ApplicationService) {
 
-    @PostMapping("/get") //TODO Long will be changed to filter object in future
+    @PostMapping("/get")
     fun getApplicationsByEmployeeId(@RequestBody pageInfo: PageInfo<ApplicationRow>): Flux<ApplicationRow> = applicationService.getAllByFilter(pageInfo);
 
-    @PostMapping("/count") //TODO PageInfo will be changed to filter object in future
+    @PostMapping("/count")
     fun getCountByEmployeeId(@RequestBody pageInfo: PageInfo<ApplicationRow>): Mono<Long> = applicationService.getCountByFilter(pageInfo)
 
 }
