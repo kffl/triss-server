1bea1d4588a29a7e1144190030db90beda81b9e6
Rybov <jankowalczyk38@gmail.com>
2020-12-04 17:41:54 +0100

    Status.kt fix

--- triss-core/src/main/kotlin/com/pp/trisscore/model/enums/Status.kt
+++ triss-core/src/main/kotlin/com/pp/trisscore/model/enums/Status.kt
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.annotation.JsonFormat
 
 @JsonFormat(shape = JsonFormat.Shape.NUMBER_INT)
 enum class Status(val namePl: String, val nameEng: String) {
-    WaitingForDirector("Oczekuje na dyrektora","Waiting for director"),
+    WaitingForDirector("Oczekuje na dyrektora","Waiting for Director"),
     WaitingForWilda("Oczekuje na Wildę","Waiting for Wilda"),
-    WaitingForRector("Oczekuje na rektora", "Waiting for rector"),
+    WaitingForRector("Oczekuje na rektora", "Waiting for Rector"),
     WaitingForWildaAgain("Oczekuje ponownie na Wildę","Waiting again for Wilda"),
     RejectedByDirector("Odrzucony przez Dyrektora","Rejected by Director"),
     RejectedByWilda("Odrzucony przez Wilde","Rejected by Wilda"),
