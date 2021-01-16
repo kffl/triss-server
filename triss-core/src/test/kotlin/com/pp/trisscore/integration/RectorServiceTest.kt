05851d05ad8b39ab2f4a559deec269d3720d27b9
Rybov <jankowalczyk38@gmail.com>
2021-01-16 19:38:47 +0100

    hotfix applications view

--- triss-core/src/test/kotlin/com/pp/trisscore/integration/RectorServiceTest.kt
+++ triss-core/src/test/kotlin/com/pp/trisscore/integration/RectorServiceTest.kt
@@ -41,8 +41,6 @@ class RectorServiceTest(@Autowired val rectorService: RectorService,
     @Test
     fun shouldGetRectorCount() {
         val x = rectorService.getCountByFilter(existingRectorToken, pageInfo).block()
-        val y = rectorService.getApplications( pageInfo,existingRectorToken).collectList().block()
-        print(y)
         assertEquals(4, x)
     }
 
